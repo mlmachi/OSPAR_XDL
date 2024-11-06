02ecf3a109561ee6eeff0d2a35b7bf937e5ae72d
mlmachi <142472870+mlmachi@users.noreply.github.com>
2024-11-06 09:35:59 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,18 @@
 pip install --use-pep -r requirements.txt
 cde data download
+
+python3 src/download_wordnet.py
+
+# Installation of XDL
+git clone https://gitlab.com/croningroup/chemputer/xdl.git
+pip install -e ./xdl
+
+# Installation of CLAIRify
 git clone https://github.com/ac-rad/xdl-generation/
 mv xdl-generation xdl_generation
+
+# Installation of brat 
+git clone https://github.com/nlplab/brat.git
+cd brat
+bash install.sh
+cd ..
